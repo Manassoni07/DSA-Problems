9f089f74665f63920927fe7869e6bad4c43862dc
Manas Soni <manassoni1311@gmail.com>
2026-06-28 13:28:55 +0530

    Time: 0 ms (100%), Space: 7.9 MB (13.44%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int kthGrammar(int n, int k) {
         if(n == 1 && k == 1) return 0;
+        // pow(2, n-1) give length of nth row
         if(k <= pow(2,n-1)/2){
             return kthGrammar(n-1,k);
         }
