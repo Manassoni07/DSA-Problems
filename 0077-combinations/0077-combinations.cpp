722c724f7ee16351b66d5378afc9b8aebd0abf0d
Manas Soni <manassoni1311@gmail.com>
2026-07-13 15:00:37 +0530

    Time: 35 ms (91.56%), Space: 62.6 MB (62.79%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    void solve(int n, int k, vector<vector<int>> &result, vector<int> &curr, int i){
+        // base case 
+        if(curr.size() == k){
+            result.push_back(curr);
+        }
+
+        for(int j = i; j <= n; j++){
+            curr.push_back(j);
+            solve(n,k,result,curr,j+1);
+            curr.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> curr;
+
+        solve(n,k,result,curr,1);
+
+        return result;
+    }
+};
