b07677f5c7ed16d0fd6b9fb32628c30b2de5eab4
Manas Soni <manassoni1311@gmail.com>
2026-07-14 15:39:19 +0530

    Time: 3 ms (27.79%), Space: 9.9 MB (60.18%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -4,15 +4,12 @@ class Solution {
             res.push_back(curr);
             return;
         }
-        // Choice 1: Don't Pick the current element
-        solve(nums, res, curr, idx + 1);
-        
-        // Choice 2: Pick the current element
+
         curr.push_back(nums[idx]);
         solve(nums, res, curr, idx + 1);
-        
-        // Backtracking step: Remove the element before returning to the previous state
+
         curr.pop_back();
+        solve(nums, res, curr, idx + 1);
         
     }
 public:
