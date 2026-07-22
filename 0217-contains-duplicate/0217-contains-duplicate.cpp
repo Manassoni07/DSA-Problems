614da0bbbb0c5f6e2e972ee49287b7aad7bb5592
Manas Soni <manassoni1311@gmail.com>
2026-07-22 22:13:21 +0530

    Time: 28 ms (83.23%), Space: 82.8 MB (82.5%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(int num : nums){
-            if(m[num] >= 1) return true;
-            m[num]++;
+        sort(nums.begin(), nums.end());
+        for(int i =0 ; i<nums.size()-1; i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
         }
         return false;
     }
