46a82402672912a0ff6fffbc0916f4a465ca7bdb
Manas Soni <manassoni1311@gmail.com>
2026-07-22 22:12:46 +0530

    Time: 75 ms (25.79%), Space: 111.4 MB (20.01%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int num : nums){
+            if(m[num] >= 1) return true;
+            m[num]++;
+        }
+        return false;
+    }
+};
