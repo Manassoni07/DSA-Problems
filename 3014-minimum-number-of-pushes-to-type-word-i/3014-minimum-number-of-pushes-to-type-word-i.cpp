13dc8e22dcb29a777dbde97824be80fd4efc154e
Manas Soni <manassoni1311@gmail.com>
2026-07-30 15:28:51 +0530

    Time: 0 ms (100%), Space: 8.4 MB (31.81%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int ans = 0;
+
+        for (int i = 0; i < word.size(); i++) {
+
+            // Every 8 letters require one extra push.
+            ans += i / 8 + 1;
+        }
+
+        return ans;
+    }
+};
