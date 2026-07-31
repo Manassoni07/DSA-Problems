3c9ac36e9f2ddaf284cd5c5f96d2cdf698e29902
Manas Soni <manassoni1311@gmail.com>
2026-07-31 16:57:59 +0530

    Time: 0 ms (100%), Space: 22.2 MB (76.75%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,28 +2,35 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
+        int i = 1;
+        int candies = n;
 
-        vector<int> count(n,1); // O(n) sc
-
-        for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i-1]){
-                count[i] = max(count[i],count[i-1] + 1);
+        while(i < n){
+            if(i < n && ratings[i] == ratings[i-1]){
+                i++;
+                continue;
             }
-        }
 
-        for(int i = n - 2; i >= 0; i--){
-            if(ratings[i] > ratings[i+1]){
-                count[i] = max(count[i],count[i+1] + 1);
+            // now we start in upwards diration 
+            int peak = 0;
+
+            while(i < n && ratings[i] > ratings[i-1]){
+                peak++;
+                candies += peak;
+                i++;
             }
+
+            // now we move in downwards diration 
+            int dip = 0;
+            while(i < n && ratings[i] < ratings[i - 1]){
+                dip++; 
+                candies += dip;
+                i++;
+            }
+
+            candies -= min(dip,peak);
+
         }
-
-        int ans = 0;
-
-        for(int x : count) ans += x;
-
-        return ans;
-
+            return candies;
     }
 };
-
-// sc O(n) 
