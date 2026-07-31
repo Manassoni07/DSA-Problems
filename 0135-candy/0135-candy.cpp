57c9f3914a300db3ec1cabb86219bfc1913b99bf
Manas Soni <manassoni1311@gmail.com>
2026-07-31 16:14:02 +0530

    Time: 0 ms (100%), Space: 22.9 MB (68.35%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        
-        vector<int> left(n,1);
-        vector<int> right(n,1);
+
+        vector<int> count(n,1); // O(n) sc
 
         for(int i = 1; i < n; i++){
             if(ratings[i] > ratings[i-1]){
-                left[i] = max(left[i],left[i-1] + 1);
+                count[i] = max(count[i],count[i-1] + 1);
             }
         }
 
         for(int i = n - 2; i >= 0; i--){
             if(ratings[i] > ratings[i+1]){
-                right[i] = max(right[i],right[i+1] + 1);
+                count[i] = max(count[i],count[i+1] + 1);
             }
         }
 
         int ans = 0;
 
-        for(int i = 0; i < n; i++){
-            ans += max(left[i],right[i]);
-        }
+        for(int x : count) ans += x;
 
         return ans;
 
     }
 };
+
+// sc O(n) 
