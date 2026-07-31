e9205f537203b1ee5ee231699811b13f7a994b78
Manas Soni <manassoni1311@gmail.com>
2026-07-31 22:06:15 +0530

    Time: 23 ms (20.74%), Space: 45.9 MB (39.71%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        sort(people.begin(), people.end());
+        int ans = 0;
+
+        int i = 0;
+        int j = n -1;
+
+        while( i <= j ){
+            if(people[i] + people[j] <= limit){
+                i++;
+                j--;
+            }
+            else{
+                j--;
+            }
+            ans += 1;
+        }
+        
+        return ans;
+    }
+};
